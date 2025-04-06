f81bd5619ce5502d8ea93a261a44f61b6b3e60c5
Maxine <mxnliao@gmail.com>
2025-04-06 12:32:54 +0800

    name fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/pulse-app-official/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/pulse-app-admin/" : "/",
   plugins: [react()],
   server: {
     open: 'index.html',
